b6eb69d389b4c5bd3b8a082d320b290d7efe2e78
Tomay91 <tomay91@gmail.com>
2020-05-16 23:58:12 -0400

    updating index file

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-
-
-
-
+const axios = require("axios");
+const dotenv = require("dotenv");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 
 const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username",
+    }
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+    }
+    {
+        type: "input",
+        message: "Give a desciption of your project",
+        name: "project",
+    }
+    {
+        type: "input",
+        message: "What is your repository name?",
+        name: "repository",
+    } 
+    ]
+
+
+}
